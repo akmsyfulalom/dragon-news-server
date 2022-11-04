023444d28b1bdb05a5db105d32530ba10ffbd570
akmsyfulalom <akmsyfulbd12@gmail.com>
2022-11-05 04:43:17 +0600

    news website

--- index.js
+++ index.js
@@ -3,16 +3,41 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
+
 const categories = require('./data/categories.json')
+
+const news = require('./data/news.json')
+
 app.get('/', (req, res) => {
     res.send('News API Running')
 });
 
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+});
+
 app.get('/news-categories', (req, res) => {
     res.send(categories)
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    } else {
+        const category_news = news.filter(n => n.category_id === id)
+        res.send(category_news)
+    }
+
 })
 app.listen(port, () => {
     console.log('Dragon news server is running', port)
-})
+});
 
